7d51e271c7a3b1a4401195f085693dd40e077c27
Olowodev <nathanielolowofoyeku@gmail.com>
2025-03-09 19:05:54 +0000

    multi page implementation

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+        cars: '/cars.html',
+        login: '/login.html',
+        signup: '/signup.html',
+        extras: '/extras.html',
+        checkout: '/checkout.html',
+        upgrade: '/upgrade.html',
+        verify: '/verify.html'
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
