7f94bfd5261b2e3aee216f740df73d1515634801
Olowodev <nathanielolowofoyeku@gmail.com>
2025-03-18 09:37:48 +0000

    nav

--- src/main.js
+++ src/main.js
@@ -7,13 +7,19 @@ import { createIcons, icons } from 'lucide';
 // Caution, this will import all the icons and bundle them.
 createIcons({ icons });
 
-// document.querySelector('#app').innerHTML = `
-//   <div className="min-h-screen flex flex-col">
-//       ${NavBar}
-//       <main className="flex-1 flex flex-col items-center justify-start">
-//         ${SignUpForm}
-//       </main>
-//     </div>
-// `
+
+// Check if token exists in localStorage
+const token = localStorage.getItem('token');
+if (token) {
+    console.log('Token found:', token);
+    
+  document.getElementById('login').style.display = 'none';
+  document.getElementById('signup').style.display = 'none'
+  document.getElementById('profile').style.display = 'block'
+    // Redirect to a protected route or perform actions that require authentication
+} else {
+    // console.log('No token found. Redirecting to login...');
+    // Redirect to login page
+}
 
 // setupCounter(document.querySelector('#counter'))
