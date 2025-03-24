2545e0a055f9f06866c3ed41b1160a3e213fc545
Olowodev <nathanielolowofoyeku@gmail.com>
2025-03-24 20:48:07 +0000

    reset password fix

--- src/resetPassword.js
+++ src/resetPassword.js
@@ -3,11 +3,15 @@ import { createIcons, icons } from 'lucide';
 // Caution, this will import all the icons and bundle them.
 createIcons({ icons });
 
+let showConfirm = false;
+let showPassword = false;
+
 const urlParams = new URLSearchParams(window.location.search);
 const token = urlParams.get('token');
 const id = urlParams.get('id');
 const errorMessageConfirmPassword = document.getElementById("confirmPasswordError");
 const errorMessagePassword = document.getElementById("passwordError");
+const submitButton = document.getElementById('submit-button')
 
 // Check if token exists in localStorage
 const localtoken = localStorage.getItem('accessToken');
@@ -26,6 +30,49 @@ if (localtoken) {
   document.getElementById('signup').style.display = 'block'
   document.getElementById('logout').style.display = 'none'
 }
+
+const confirmEyeButton = document.getElementById('confirm-eye-button')
+const eyeButton = document.getElementById('eye-button')
+const confirmEye = document.getElementById('confirm-eye')
+const eye = document.getElementById('eye')
+const confirmEyeOff = document.getElementById('confirm-eye-off')
+const eyeOff = document.getElementById('eye-off')
+const confirmPassword = document.getElementById('confirmPassword')
+const password = document.getElementById('password')
+
+const changeConfirmState = () => {
+  if (showConfirm) {
+      confirmPassword.type = 'text'
+      confirmEye.classList.remove('hidden')
+      confirmEyeOff.classList.add('hidden')
+  } else {
+      confirmPassword.type = 'password'
+      confirmEyeOff.classList.remove('hidden')
+      confirmEye.classList.add('hidden')
+  }
+}
+
+const changePasswordState = () => {
+  if (showPassword) {
+      password.type = 'text'
+      eye.classList.remove('hidden')
+      eyeOff.classList.add('hidden')
+  } else {
+      password.type = 'password'
+      eyeOff.classList.remove('hidden')
+      eye.classList.add('hidden')
+  }
+}
+
+confirmEyeButton.addEventListener('click', () => {
+  showConfirm = !showConfirm;
+  changeConfirmState()
+})
+
+eyeButton.addEventListener('click', () => {
+  showPassword = !showPassword;
+  changePasswordState()
+})
 
 // Function to validate password
 const isValidPassword = (password) => {
